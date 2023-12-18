98c12b07c1caea822b706f19aadd05cf4dc2bd19
Aigerim-vebka <120300410+Aigerim-vebka@users.noreply.github.com>
2023-12-18 14:19:14 +0600

    add class question and object class Constants

--- app/src/main/java/com/example/moviegame/screens/Constant.kt
+++ app/src/main/java/com/example/moviegame/screens/Constant.kt
@@ -7,75 +7,76 @@ object Constants {
         val questionsList = ArrayList<Question>()
 
         val que1 = Question(1,
-                "What country flag is this?",
-                R.drawable.australia,
-                "Australia",
-                "Argentina",
+                "How is the first Kazakh film nominated for the Golden Palm wind Cannes Film Festival?",
+                R.drawable.kelin,
+                "Kelin",
+                "",
                 "Armenia", "USA",
                 1)
 
 
 
         val que2 = Question(2,
-                "What country flag is this", R.drawable.mexico,
-                "Australia",
-                "Argentina",
-                "Mexico",
-                "USA",
+                "Who played the role of Harley Quinn in the movie(2016)?",
+                R.drawable.margot,
+                "Scarlett Johansson",
+                "Gal Gadot",
+                "Margot Robbie",
+                "Emma Stone",
                 3)
 
 
         val que3 = Question(3,
-                "What country flag is this", R.drawable.france,
-                "Mexico",
-                "France",
-                "Africa",
-                "USA",
+                "Which movie didn't have Winona Ryder in it?", R.drawable.grape,
+                "Lucas",
+                "What's eating Gilbert Grape?",
+                "Dracula",
+                "Interrupted life",
                 2)
 
 
         val que4 = Question(4,
-                "What country flag is this", R.drawable.turkey,
-                "Kazakistan",
-                "Ukraine",
-                "Turkey",
-                "USA",
+                "Which actress has won the most Oscars?", R.drawable.katharine,
+                "Meryl Streep",
+                "Ingrid Bergman",
+                "Katharine Hepburn",
+                "Jimmy Fallon",
                 3)
 
 
         val que5 = Question(5,
-                "What country flag is this", R.drawable.us,
-                "USA",
-                "Argentina",
-                "Armenia",
-                "South America",
+                "On the set of which movie did Miley Cyrus and Liam Hemsworth first meet?", R.drawable.song,
+                "The last song",
+                "The Hunger Games",
+                "Quiet Harbor",
+                "The Bourne Supremacy",
                 1)
 
 
         val que6 = Question(6,
-                "What country flag is this", R.drawable.uk,
-                "Australia",
-                "Argentina",
-                "UK",
-                "USA",
+                "Which child was not on a trip to the Wonka Factory in the movie Willy Wonka and the Chocolate Factory?", R.drawable.billy,
+                "Mike Teavee",
+                "Veruki Salt",
+                "Billy the Warp",
+                "Charlie Bucket",
                 3)
 
 
         val que7 = Question(7,
-                "What country flag is this", R.drawable.european,
-                "Scotland",
-                "European Union",
-                "Armenia",
-                "USA",
+                "How many Oscars has Halle Berry won?", R.drawable.one,
+                "2",
+                "1",
+                "4",
+                "3",
                 2)
 
 
         val que8 = Question(8,
-                "What country flag is this", R.drawable.germany,
-                "Netherlands",
-                "Spain",
-                "Belgium",
-                "Germany",
+                "Who actually painted the Rose in Titanic?", R.drawable.james,
+                "Leonardo DiCaprio",
+                "Billy Zane",
+                "Angelina Jolie",
+                " James Cameron",
                 4)
 
 
@@ -111,7 +112,4 @@ object Constants {
 
 
 
-        return questionsList
-    }
-
-}
+        return quest
